57b5f9f0bd06290cdfd3d2e5b21d8721814f1c4b
leegakyeong <alaska7132@naver.com>
2022-10-06 15:00:30 +0900

    Create Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,30 @@
+<script>
+  import logoWhite from '../assets/logo-white.svg'
+</script>
+
+<footer>
+  <img src={logoWhite} alt="" width="70" height="auto" />
+  <nav>
+    <a href="https://www.instagram.com/with.hangul">
+      <img src="" alt="인스타그램" />
+    </a>
+    <a href="https://www.facebook.com/with.hangul">
+      <img src="" alt="페이스북" />
+    </a>
+  </nav>
+</footer>
+
+<style>
+  footer {
+    height: 130px;
+    background-color: black;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  footer > img {
+    margin-left: 30px;
+  }
+</style>
