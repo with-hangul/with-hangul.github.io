75e828179047a187e04aa5d0fa2144eb93c472e8
leegakyeong <alaska7132@naver.com>
2022-10-06 15:35:59 +0900

    Update Vision | Add titles and footer

---
+++ src/pages/Vision.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<main>
+  <section>
+    <div class="title">비전</div>
+    <div></div>
+  </section>
+  <section>
+    <div class="title">관심사</div>
+    <div></div>
+  </section>
+  <section>
+    <div class="title">일하는 방식</div>
+    <div></div>
+  </section>
+  <Footer />
+</main>
+
+<style>
+  .title {
+    background-color: #AC9AFF;
+    height: 160px;
+    font-weight: 700;
+    font-size: 100px;
+    color: black;
+    border-bottom: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
