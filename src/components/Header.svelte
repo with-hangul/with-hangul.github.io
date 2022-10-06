d7fd9ccfe896595c2f748f7b651e785ae555908d
leegakyeong <alaska7132@naver.com>
2022-10-06 14:52:54 +0900

    Create Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { link } from 'svelte-spa-router'
+  import logo from '../assets/logo.svg'
+</script>
+
+<header>
+  <a href="/" use:link>
+    <img id="logo" src={logo} alt="" width="auto" height="75%" />
+  </a>
+  <nav>
+    <a href="/about" use:link>ABOUT</a>
+    <a href="/vision" use:link>VISION</a>
+    <a href="/projects" use:link>PROJECTS</a>
+  </nav>
+</header>
+
+<style>
+  header {
+    height: 50px;
+    border-bottom: 2px solid black;
+    display: flex;
+    justify-content: space-between;
+    /* box-sizing: border-box; */
+    position: fixed;
+    top: 0px;
+    width: 100%;
+    background-color: white;
+  }
+
+  a {
+    display: inline-flex;
+    height: 50px;
+    text-decoration: none;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #logo {
+    margin-left: 8px;
+  }
+
+  nav {
+    display: inline-flex;
+    justify-content: space-between;
+  }
+
+  nav > a {
+    border-left: 2px solid black;
+    width: 150px;
+    background-color: white;
+    color: black;
+    font-weight: 300;
+    font-size: 20px;
+  }
+
+  nav > a:hover {
+    background-color: black;
+    color: white;
+  }
+</style>
