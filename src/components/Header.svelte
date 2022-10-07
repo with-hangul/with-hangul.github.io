ba8671496853bc45c54400812e2aafadaf94fbca
leegakyeong <alaska7132@naver.com>
2022-10-07 17:36:11 +0900

    Update Header | Add underline to selected nav button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,16 +1,34 @@
 <script>
-  import { link } from 'svelte-spa-router'
+  import { onMount } from 'svelte'
+  import { link, location } from 'svelte-spa-router'
   import logo from '../assets/logo.svg'
+
+  onMount(() => {
+    const currentPage = $location.substring(1).toUpperCase()
+
+    document.getElementsByTagName('nav')[0].children.forEach((page) => {
+      if (page.innerText === currentPage) page.style.textDecoration = 'underline'
+    })
+  })
+
+  function select(e) {
+    // e.target.classList.add('selected')
+    e.target.style.textDecoration = 'underline'
+    document.querySelectorAll('a').forEach((link) => {
+      // if (sibling !== e.target) sibling.classList.remove('selected')
+      if (link !== e.target) link.style.textDecoration = 'none'
+    })
+  }
 </script>
 
 <header>
-  <a href="/" use:link>
+  <a href="/" use:link on:click={select}>
     <img id="logo" src={logo} alt="" width="auto" height="75%" />
   </a>
   <nav>
-    <a href="/about" use:link>ABOUT</a>
-    <a href="/vision" use:link>VISION</a>
-    <a href="/projects" use:link>PROJECTS</a>
+    <a href="/about" use:link on:click={select}>ABOUT</a>
+    <a href="/vision" use:link on:click={select}>VISION</a>
+    <a href="/projects" use:link on:click={select}>PROJECTS</a>
   </nav>
 </header>
 
@@ -57,5 +75,9 @@
   nav > a:hover {
     background-color: black;
     color: white;
+  }
+
+  nav > a.selected {
+    text-decoration: underline;
   }
 </style>
