83637227e16199a4afb290f24b4b728c8181d16d
leegakyeong <alaska7132@naver.com>
2022-10-07 17:01:01 +0900

    Update About | Add history graph

---
+++ src/components/History.svelte
@@ -0,0 +1,90 @@
+<div id="history" class="center">
+  <!-- 이런 그래프를 뭐라고 부르더라....?? -->
+  <div class="leaves left">
+    <div class="leaf">
+      <div class="year">2018</div>
+      <div class="title">학내 해커톤 1위</div>
+      <div class="description">서울대학교 웹 개발 동아리<br />'멋쟁이 사자처럼' 6기</div>
+    </div>
+    <div class="leaf"></div>
+    <div class="leaf">
+      <div class="year">2019</div>
+      <div class="title">글자랑 데모 영상 공개</div>
+    </div>
+    <div class="leaf"></div>
+    <div class="leaf">
+      <div class="year">2022</div>
+      <div class="title">예비창업패키지 선정</div>
+      <div class="description">주관 : 세종창조경제혁신센터</div>
+    </div>
+    <div class="leaf"></div>
+    <div class="leaf">
+      <div class="title">아울 OWL 오픈 베타테스트 (예정)</div>
+      <div class="description">웹 폰트 병합 서비스</div>
+    </div>
+  </div>
+  <div class="stem"></div>
+  <div class="leaves right">
+    <div class="leaf"></div>
+    <div class="leaf">
+      <div class="title">전시 참여 &lt;한옥한글&gt;</div>
+      <div class="description">남산골한옥마을 기획전</div>
+    </div>
+    <div class="leaf"></div>
+    <div class="leaf">
+      <div class="year">2020</div>
+      <div class="title">학회지 논문 게재</div>
+      <div class="description">한글타이포그라피학회<br />한글 폰트 디자인 인터페이스</div>
+    </div>
+    <div class="leaf"></div>
+    <div class="leaf">
+      <div class="title">3, 2, wrm, Action!</div>
+      <div class="description">프로젝트 선정<br />후원사 : wrm</div>
+    </div>
+    <div class="leaf"></div>
+  </div>
+</div>
+
+<style>
+  #history {
+    display: grid;
+    grid-template-columns: 1fr 10px 1fr;
+    grid-template-rows: repeat(7);
+    column-gap: 10px;
+  }
+
+  .stem {
+    width: 10px;
+    height: 100%;
+    background-color: #673EFF;
+  }
+
+  .leaves.left .leaf * {
+    text-align: right;
+  }
+
+  .leaves.right .leaf * {
+    text-align: left;
+  }
+
+  .leaf {
+    height: 100px;
+  }
+
+  .leaf .year {
+    font-size: 25px;
+    font-weight: 700;
+    color: #673EFF;
+  }
+
+  .leaf .title {
+    font-size: 25px;
+    font-weight: 700;
+    color: black;
+  }
+
+  .leaf .description {
+    font-size: 25px;
+    color: black;
+  }
+</style>
--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,5 +1,6 @@
 <script>
   import Footer from '../components/Footer.svelte';
+  import History from '../components/History.svelte';
   import BusinessCard from '../components/BusinessCard.svelte';
   import businesses from '../businesses';
 </script>
@@ -11,14 +12,17 @@
     <div class="heading">글자로 디지털 세상을 이롭게</div>
     <p>글자랑은 서체의 디지털화·시스템화·대중화에 대한 솔루션을 고민하는 스타트업입니다. 현재는 UX, 웹개발, 서체 디자인 분야의 전문가가 모여 새로운 웹서비스를 개발하고 있습니다.
       <br /><br />
-      한글 서체 창작자와 사용자들이 오늘날의 디지털 타이포그래피 환경에서 겪는 크고 작은 불편들을 해결하고, 창작자의 워크플로우와 밀접한 서체 솔루션을 만듭니다.</p>
+      한글 서체 창작자와 사용자들이 오늘날의 디지털 타이포그래피 환경에서 겪는 크고 작은 불편들을 해결하고, 창작자의 워크플로우와 밀접한 서체 솔루션을 만듭니다.
+    </p>
   </section>
   <section id="history">
     <div class="subheading">History</div>
     <div class="heading">디자인과 기술의 만남</div>
     <p>이 아이디어는 한글타이포학교의 활자 디자인 과정 수강생의 작은 게으름에서 출발했습니다. 서울대학교 웹 개발 동아리 ‘멋쟁이 사자처럼’에서 첫 아이디어 피칭을 시작으로, 글자에 관심을 가진 사람들이 모였습니다.
       <br /><br />
-      작지만 강한 아이디어가 서비스로 거듭날 수 있도록 2022년 글자랑은 기업의 형태로 탈바꿈합니다. 이제 새로운 사업 기회에 도전하며, 폰트 창작자들의 생존도 보장할 수 있는 팀으로 거듭나겠습니다.</p>
+      작지만 강한 아이디어가 서비스로 거듭날 수 있도록 2022년 글자랑은 기업의 형태로 탈바꿈합니다. 이제 새로운 사업 기회에 도전하며, 폰트 창작자들의 생존도 보장할 수 있는 팀으로 거듭나겠습니다.
+    </p>
+    <History />
   </section>
   <section id="business">
     <div class="subheading">Business</div>
@@ -95,6 +99,7 @@
     padding-top: 102px;
     padding-bottom: 184px;
     padding-left: 83px;
+    padding-right: 83px;
   }
 
   #history .subheading {
@@ -110,6 +115,7 @@
   #history p {
     color: black;
     width: 800px;
+    margin-bottom: 145px;
   }
 
   #business {
