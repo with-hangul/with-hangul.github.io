fc4d9acb1d8d230cc937714609230ef5c3752bf0
leegakyeong <alaska7132@naver.com>
2022-10-07 10:22:56 +0900

    Update Home | Add contents

---
+++ src/pages/Home.svelte
@@ -0,0 +1,59 @@
+<script>
+  import Footer from '../components/Footer.svelte';
+</script>
+
+<main>
+  <section id="first">
+    <img src="" />
+  </section>
+  <section id="second">
+    <div class="vertical-container">
+      <img src="" />
+      <div>새로운 글자를 쉽고, 빠르게</div>
+    </div>
+  </section>
+  <section id="third"></section>
+  <section id="fourth">
+    <div>사람</div>
+    <div>글자</div>
+    <div>기술</div>
+  </section>
+  <section id="fifth">
+    <div>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+    </div>
+    <div>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+      <span>글자랑</span>
+    </div>
+  </section>
+  <section id="sixth">
+    <div>
+      <div>글자:랑</div>
+      <div>글자와 함께하다</div>
+    </div>
+    <div>
+      <div>글:자랑</div>
+      <div>글을 자랑하다</div>
+    </div>
+  </section>
+  <section id="seventh"></section>
+  <Footer />
+</main>
