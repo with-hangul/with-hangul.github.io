fbca8485e9337c474f88396b5903ab3c9355c0a9
leegakyeong <alaska7132@naver.com>
2022-10-07 11:21:34 +0900

    Update Home | Style remaining sections

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -20,7 +20,7 @@
     <div class="keyword center">글자</div>
     <div class="keyword center">기술</div>
   </section>
-  <section class="marquee" style="height: 160px; background-color: black; color: white;">
+  <section class="marquee" style="height: unset; background-color: black; color: white;">
     <div class="marquee-content">
       <span>글자랑</span>
       <span>글자랑</span>
@@ -46,17 +46,17 @@
       <span>글자랑</span>
     </div>
   </section>
-  <section id="sixth">
-    <div>
+  <section class="border-bottom center" style="background-color: #FED35D; height: unset;">
+    <div class="center meaning">
       <div>글자:랑</div>
       <div>글자와 함께하다</div>
     </div>
-    <div>
+    <div class="center meaning">
       <div>글:자랑</div>
       <div>글을 자랑하다</div>
     </div>
   </section>
-  <section id="seventh"></section>
+  <section style="height: 460px; background-color: #F4F4F0;"></section>
   <Footer />
 </main>
 
@@ -83,7 +83,7 @@
   }
 
   .marquee-content {
-    height: 100%;
+    height: 160px;
     display: flex;
     align-items: center;
     flex-shrink: 0;
@@ -106,5 +106,24 @@
     100% {
       transform: translateX(0%);
     }
+  }
+
+  .meaning {
+    flex: 1;
+    flex-direction: column;
+    height: 250px;
+  }
+
+  .meaning:first-child {
+    border-right: 2px solid black;
+  }
+
+  .meaning > div:first-child {
+    font-weight: 600;
+    font-size: 50px;
+  }
+
+  .meaning >div:nth-child(2) {
+    font-size: 30px;
   }
 </style>
