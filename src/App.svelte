92c7c568f7e8c35e2b88ef876ad8a12709de1f65
leegakyeong <alaska7132@naver.com>
2022-10-07 13:23:43 +0900

    Add About and Projects page

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,15 @@
   import Router from 'svelte-spa-router'
   import Header from './components/Header.svelte'
   import Home from './pages/Home.svelte'
+  import About from './pages/About.svelte'
   import Vision from './pages/Vision.svelte'
+  import Projects from './pages/Projects.svelte'
 
   const routes = {
     '/': Home,
+    '/about': About,
     '/vision': Vision,
+    '/projects': Projects,
   }
 </script>
 
---
+++ src/pages/Projects.svelte
@@ -0,0 +1,3 @@
+<main>
+  <div>projects</div>
+</main>
